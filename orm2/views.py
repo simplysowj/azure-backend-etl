8ee0d1c09028255980bbd404b2e8ade756f811be
sowjanya <90475451+simplysowj@users.noreply.github.com>
2025-03-28 16:10:50 -0400

    Update views.py

--- orm2/views.py
+++ orm2/views.py
@@ -15,6 +15,8 @@ import mysql.connector
 import openai
 from openai import OpenAI
 OpenAI.api_key ="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"
+
+OpenAI.api_key = os.getenv('OPENAI_API_KEY') 
 client = OpenAI()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')  
